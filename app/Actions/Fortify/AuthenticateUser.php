be8a5735651a1341b0060a2fc134e17c52628a56
Patrizia Rubini <patriziarubini1976@gmail.com>
2026-05-09 21:52:23 +0200

    mod txt

--- app/Actions/Fortify/AuthenticateUser.php
+++ app/Actions/Fortify/AuthenticateUser.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions\Fortify;
 
+use Illuminate\Support\Facades\Log;
 use Laravel\Fortify\Http\Requests\LoginRequest;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -15,16 +16,29 @@ class AuthenticateUser
         $user = \App\Models\User::where('email', $credentials['email'])->first();
 
         if ($user && $user->salt) {
+
             $pepper = config('app.pepper');
 
             $passwordWithSaltPepper =
                 $credentials['password'] . $user->salt . $pepper;
 
             if (Hash::check($passwordWithSaltPepper, $user->password)) {
+
                 Auth::login($user);
+
+                Log::info('User logged in', [
+                    'email' => $user->email,
+                    'ip' => $request->ip(),
+                ]);
+
                 return $user;
             }
         }
+
+        Log::warning('Failed login attempt', [
+            'email' => $credentials['email'] ?? null,
+            'ip' => $request->ip(),
+        ]);
 
         return null;
     }
